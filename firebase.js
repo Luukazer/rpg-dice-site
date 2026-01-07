506ada4faa0f84edad2e484c22f30eb508e51999
Luukazer <95034301+Luukazer@users.noreply.github.com>
2026-01-06 23:31:20 -0300

    firebase.js

---
+++ firebase.js
@@ -0,0 +1,16 @@
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
+<script>
+const firebaseConfig = {
+  apiKey: "AIzaSyBzniGkaFn9HzyeS9XhQ9cAETXWIPFcsQw",
+  authDomain: "sociedade-mitra-dados.firebaseapp.com",
+  databaseURL: "https://sociedade-mitra-dados-default-rtdb.firebaseio.com",
+  projectId: "sociedade-mitra-dados",
+  storageBucket: "sociedade-mitra-dados.appspot.com",
+  messagingSenderId: "155136545326",
+  appId: "1:155136545326:web:b9721595277fb7cef685bf"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
+</script>
