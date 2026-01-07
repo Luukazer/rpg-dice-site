d227a6c380789ebf477c4899dbd312c7ffd37c09
Luukazer <95034301+Luukazer@users.noreply.github.com>
2026-01-06 23:42:01 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
 <script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-database-compat.js"></script>
 <script>
-  firebase.initializeApp({...});
-  const db = firebase.database();
+const firebaseConfig = {
+  apiKey: "AIzaSyBzniGkaFn9HzyeS9XhQ9cAETXWIPFcsQw",
+  authDomain: "sociedade-mitra-dados.firebaseapp.com",
+  databaseURL: "https://sociedade-mitra-dados-default-rtdb.firebaseio.com",
+  projectId: "sociedade-mitra-dados",
+  storageBucket: "sociedade-mitra-dados.appspot.com",
+  messagingSenderId: "155136545326",
+  appId: "1:155136545326:web:b9721595277fb7cef685bf"
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
 </script>
